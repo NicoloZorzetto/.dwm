cc583266999ce96d9f32667657b73e5ac02f837e
NicoloZorzetto <nicolozorzetto@protonmail.com>
2021-02-07 14:36:31 +0100

    Blackout, standard keybindings

---
+++ scripts/autostart.sh
@@ -0,0 +1,2 @@
+xrandr --output HDMI-A-0 --rotate left
+feh --bg-fill /home/nicolo/images/black_abstract_pin_dunes.jpe
---
+++ scripts/feh-wallpaper-setting.sh
@@ -0,0 +1,1 @@
+feh --bg-fill /home/nicolo/images/wallpapers/black_abstract_pin_dunes.jpe
